842d815aed46b8e82e40bb989760f222aeefbe70
SPGoding <SPGoding@outlook.com>
2017-12-31 12:46:01 +0800

    :truck:Rename files.

---
+++ src/Forms/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DataMaker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.menuTop = new System.Windows.Forms.MenuStrip();
+            this.smnuDataPack = new System.Windows.Forms.ToolStripMenuItem();
+            this.smnuLoadFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new DataMaker.BetterControls.BetterToolStripSeparator();
+            this.smnuSaveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new DataMaker.BetterControls.BetterToolStripSeparator();
+            this.smnuUnzip = new System.Windows.Forms.ToolStripMenuItem();
+            this.smnuExportZip = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new DataMaker.BetterControls.BetterToolStripSeparator();
+            this.smnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.smnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.smnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuTop
+            // 
+            resources.ApplyResources(this.menuTop, "menuTop");
+            this.menuTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smnuDataPack,
+            this.smnuHelp});
+            this.menuTop.Name = "menuTop";
+            // 
+            // smnuDataPack
+            // 
+            resources.ApplyResources(this.smnuDataPack, "smnuDataPack");
+            this.smnuDataPack.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smnuLoadFolder,
+            this.toolStripSeparator1,
+            this.smnuSaveFile,
+            this.toolStripSeparator2,
+            this.smnuUnzip,
+            this.smnuExportZip,
+            this.toolStripSeparator3,
+            this.smnuExit});
+            this.smnuDataPack.Name = "smnuDataPack";
+            // 
+            // smnuLoadFolder
+            // 
+            resources.ApplyResources(this.smnuLoadFolder, "smnuLoadFolder");
+            this.smnuLoadFolder.Name = "smnuLoadFolder";
+            this.smnuLoadFolder.Click += new System.EventHandler(this.smnuLoadFolder_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            // 
+            // smnuSaveFile
+            // 
+            resources.ApplyResources(this.smnuSaveFile, "smnuSaveFile");
+            this.smnuSaveFile.Name = "smnuSaveFile";
+            // 
+            // toolStripSeparator2
+            // 
+            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            // 
+            // smnuUnzip
+            // 
+            resources.ApplyResources(this.smnuUnzip, "smnuUnzip");
+            this.smnuUnzip.Name = "smnuUnzip";
+            // 
+            // smnuExportZip
+            // 
+            resources.ApplyResources(this.smnuExportZip, "smnuExportZip");
+            this.smnuExportZip.Name = "smnuExportZip";
+            // 
+            // toolStripSeparator3
+            // 
+            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            // 
+            // smnuExit
+            // 
+            resources.ApplyResources(this.smnuExit, "smnuExit");
+            this.smnuExit.Name = "smnuExit";
+            this.smnuExit.Click += new System.EventHandler(this.smnuExit_Click);
+            // 
+            // smnuHelp
+            // 
+            resources.ApplyResources(this.smnuHelp, "smnuHelp");
+            this.smnuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smnuAbout});
+            this.smnuHelp.Name = "smnuHelp";
+            // 
+            // smnuAbout
+            // 
+            resources.ApplyResources(this.smnuAbout, "smnuAbout");
+            this.smnuAbout.Name = "smnuAbout";
+            this.smnuAbout.Click += new System.EventHandler(this.smnuAbout_Click);
+            // 
+            // folderBrowser
+            // 
+            resources.ApplyResources(this.folderBrowser, "folderBrowser");
+            // 
+            // Main
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.menuTop);
+            this.IsMdiContainer = true;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuTop;
+            this.Name = "Main";
+            this.Text = this.ProductName;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.RespondShortcutKeys);
+            this.menuTop.ResumeLayout(false);
+            this.menuTop.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuTop;
+        private System.Windows.Forms.ToolStripMenuItem smnuDataPack;
+        private System.Windows.Forms.ToolStripMenuItem smnuUnzip;
+        private System.Windows.Forms.ToolStripMenuItem smnuLoadFolder;
+        private System.Windows.Forms.ToolStripMenuItem smnuExit;
+        private BetterControls.BetterToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem smnuExportZip;
+        private BetterControls.BetterToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem smnuSaveFile;
+        private BetterControls.BetterToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem smnuHelp;
+        private System.Windows.Forms.ToolStripMenuItem smnuAbout;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+    }
+}
+
---
+++ src/Forms/MainForm.cs
@@ -0,0 +1,209 @@
+﻿using DataMaker.Properties;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DataMaker
+{
+    public struct Theme
+    {
+        public static Color PressedColor = Color.FromArgb(27, 27, 28);
+        public static Color BackColor = Color.FromArgb(37, 37, 37);
+        public static Color ForeColor = Color.FromArgb(245, 245, 245);
+        public static Color HoverColor = Color.FromArgb(51, 51, 52);
+        public static Font Font = new Font("微软雅黑", 10f, GraphicsUnit.Point);
+
+        public static void Initialize(ContainerControl container)
+        {
+            container.BackColor = BackColor;
+            container.ForeColor = ForeColor;
+            container.Font = Font;
+            foreach (Control control in container.Controls)
+            {
+                if (control is ContainerControl)
+                {
+                    Initialize(control as ContainerControl);
+                }
+                else
+                {
+                    control.BackColor = BackColor;
+                    control.ForeColor = ForeColor;
+                    control.Font = Font;
+                }
+            }
+        }
+    }
+
+    public partial class Main : Form
+    {
+        #region 公共函数
+        /// <summary>
+        /// 依据指定key从资源文件读取文字
+        /// </summary>
+        /// <param name="key">指定key</param>
+        public static string Lang(string key)
+        {
+
+            var rm = new System.Resources.ResourceManager("DataMaker.Languages." + "zh_cn", typeof(Resources).Assembly);
+
+            if (rm.GetString(key) != null)
+            {
+                return rm.GetString(key);
+            }
+
+            throw new ApplicationException("No Lang: " + key);
+        }
+        #endregion
+
+        #region 设置主题样式
+        private void SetTheme()
+        {
+            Theme.Initialize(this);
+            menuTop.Renderer = new ThemeRenderer();
+
+            foreach (ToolStripItem i in menuTop.Items)
+            {
+                i.BackColor = BackColor;
+                i.ForeColor = ForeColor;
+            }
+            foreach (ToolStripItem i in smnuDataPack.DropDownItems)
+            {
+                i.BackColor = BackColor;
+                i.ForeColor = ForeColor;
+            }
+            foreach (ToolStripItem i in smnuHelp.DropDownItems)
+            {
+                i.BackColor = BackColor;
+                i.ForeColor = ForeColor;
+            }
+        }
+
+        private class ThemeRenderer : ToolStripProfessionalRenderer
+        {
+            public ThemeRenderer() : base(new ProfessionalColorTable()) { }
+
+            protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs e)
+            {
+                var rc = new Rectangle(Point.Empty, e.Item.Size);
+                if (e.Item.Selected)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Theme.HoverColor), rc);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Theme.BackColor), rc);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 重载OnLoad 去边框
+        /// FIXME: 必须要最大化时启动才有效…
+        /// </summary>
+        protected override void OnLoad(EventArgs e)
+        {
+            // https://stackoverflow.com/questions/7752696/how-to-remove-3d-border-sunken-from-mdiclient-component-in-mdi-parent-form
+            var mdiclient = Controls.OfType<MdiClient>().Single();
+            SuspendLayout();
+            mdiclient.SuspendLayout();
+            var hdiff = mdiclient.Size.Width - mdiclient.ClientSize.Width;
+            var vdiff = mdiclient.Size.Height - mdiclient.ClientSize.Height;
+            var size = new Size(mdiclient.Width + hdiff, mdiclient.Height + vdiff);
+            var location = new Point(mdiclient.Left - (hdiff / 2), mdiclient.Top - (vdiff / 2));
+            mdiclient.Dock = DockStyle.None;
+            mdiclient.Size = size;
+            mdiclient.Location = location;
+            mdiclient.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom;
+            mdiclient.ResumeLayout(true);
+            ResumeLayout(true);
+            base.OnLoad(e);
+        }
+        #endregion
+
+        private FileTree fileTree;
+
+        public Main()
+        {
+            InitializeComponent();
+            SetTheme();
+
+            // 显示文件树
+            fileTree = new FileTree()
+            {
+                MdiParent = this,
+                Dock = DockStyle.Right
+            };
+
+            fileTree.Show();
+        }
+
+        #region 事件处理
+        private void RespondShortcutKeys(object sender, KeyEventArgs e)
+        {
+            if (e.Alt)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.F4:
+                        e.SuppressKeyPress = true;
+                        ExitApplication();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (e.Control)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.D:
+                        e.SuppressKeyPress = true;
+
+                        break;
+                    case Keys.E:
+                        e.SuppressKeyPress = true;
+
+                        break;
+                    case Keys.L:
+                        e.SuppressKeyPress = true;
+                        SelectDatapackFolder();
+                        break;
+                    case Keys.S:
+                        e.SuppressKeyPress = true;
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void ShowAboutBox()
+        {
+            new AboutBox().Show();
+        }
+
+        private static void ExitApplication()
+        {
+            Application.Exit();
+        }
+
+        private void SelectDatapackFolder()
+        {
+            var result = folderBrowser.ShowDialog();
+            if (result != DialogResult.Cancel)
+            {
+                fileTree.Datapack = folderBrowser.SelectedPath;
+            }
+        }
+
+        #region 响应事件
+        private void smnuExit_Click(object sender, EventArgs e) => ExitApplication();
+        private void smnuAbout_Click(object sender, EventArgs e) => ShowAboutBox();
+        private void smnuLoadFolder_Click(object sender, EventArgs e) => SelectDatapackFolder();
+        #endregion
+        #endregion
+
+    }
+}
