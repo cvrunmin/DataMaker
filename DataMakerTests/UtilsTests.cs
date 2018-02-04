ca318c8b7c99a66bab2441fe624ef6b230ead572
SPGoding <SPGoding@outlook.com>
2018-02-04 11:45:02 +0800

    :white_check_mark:Add tests for method "Lang".

---
+++ DataMakerTests/UtilsTests.cs
@@ -0,0 +1,31 @@
+﻿using DataMaker;
+using NUnit.Framework;
+using System;
+
+namespace DataMakerTests
+{
+    [TestFixture]
+    public class UtilsTests
+    {
+        // Tests in this class doesn't need arrange.
+        // Just act and then assert.
+
+        [Test]
+        public void Lang_ValidKey_Returns()
+        {
+            var result = Utils.Lang("global_block");
+
+            Assert.AreEqual("方块", result);
+        }
+
+        [Test]
+        public void Lang_InvalidKey_Throws()
+        {
+            var ex = Assert.Catch<Exception>(() => Utils.Lang("#@$(*&"));
+
+            StringAssert.Contains("No Lang", ex.Message);
+        }
+
+
+    }
+}
